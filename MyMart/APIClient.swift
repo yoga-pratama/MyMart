5ceb0a3f3150c181ce247a0d2919fb1ca325b69a
yoga <yogamadness@gmail.com>
2018-11-22 09:30:31 +0700

    API Data

--- MyMart/APIClient.swift
+++ MyMart/APIClient.swift
@@ -12,4 +12,29 @@ typealias FilmListJSON = [String:Any]
 
 struct APIClient {
     
+    static func getFilmListAPI(completion : @escaping (FilmListJSON?) -> Void){
+        let url = URL(string: "https://rss.itunes.apple.com/api/v1/id/movies/top-movies/all/25/explicit.json")
+        
+        let session = URLSession.shared
+        
+        guard let unwrappedURL = url else{ print("Error unwrapping URL"); return}
+        
+        let dataTask = session.dataTask(with: unwrappedURL){(data , response, error) in
+            
+            guard let unwrappedData = data else { print("Error unwrapping data"); return}
+            
+            do {
+                let responseJSON = try JSONSerialization.jsonObject(with: unwrappedData, options: []) as? FilmListJSON
+                
+                
+                completion(responseJSON)
+            } catch {
+                print("Could not get API data. \(error), \(error.localizedDescription)")
+            }
+            
+           
+        }
+        dataTask.resume()
+    }
+    
 }
---
+++ MyMart/DataStore.swift
@@ -0,0 +1,52 @@
+//
+//  DataStore.swift
+//  MyMart
+//
+//  Created by Yoga Pratama on 22/11/18.
+//  Copyright © 2018 YPA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class DataStore{
+    static let sharedInstance = DataStore()
+    fileprivate init(){}
+    
+    var filmlists : [FilmList] = []
+    var images: [UIImage] = []
+    
+    func getFilm(completion: @escaping () -> Void){
+        APIClient.getFilmListAPI{ (json) in
+            let feed = json?["feed"] as? FilmListJSON
+            if let results = feed?["results"] as? [FilmListJSON]{
+                for dict in results{
+                    let newFilmlist =  FilmList(dictionary: dict)
+                    self.filmlists.append(newFilmlist)
+                }
+                completion()
+            }
+        }
+    }
+    
+    
+    
+    func getFilmPoster(completion : @escaping () -> Void){
+        getFilm {
+            for film in self.filmlists{
+                let url = URL(string: film.converImage)
+                
+                let data = try? Data(contentsOf: url!)
+                if let imageData = data{
+                     print("getting images...")
+                     let image = UIImage(data: imageData)
+                     self.images.append(image!)
+                }
+            }
+            
+            OperationQueue.main.addOperation {
+                completion()
+            }
+        }
+    }
+}
